d86b8f160cb2078d2574e5993998833a88c8e1bd
Weiyi Ma <ma823285807@gmail.com>
2019-11-09 21:49:17 +0800

    规划基本功能

--- src/telegrambot/bot.ts
+++ src/telegrambot/bot.ts
@@ -1,7 +1,9 @@
+import * as path from 'path';
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const TOKEN = process.env.TOKEN;
-
+console.log(TOKEN);
 const bot = new TelegramBot(`${TOKEN}`, { polling: true });
 
 export default bot;
