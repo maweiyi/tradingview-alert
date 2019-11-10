70759b140f07cd9d6d5315b232df89779e9a380a
Weiyi Ma <ma823285807@gmail.com>
2019-11-10 15:57:03 +0800

    add production https

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ import * as AlertRouter from './routes/alert';
 const app = express();
 
 app.use(express.json());
+app.use('/ping', function(req, res, next) {
+  res.send('pong');
+});
 app.use('/alert', AlertRouter.router);
+
+app.use(function(req, res, next) {
+  res.status(404).send('Not Found');
+});
+
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.send(err);
+  }
+});
 
 export default app;
