6b2f0578aaefffbb4320f5379cc6a5cd6c2c44be
Weiyi Ma <ma823285807@gmail.com>
2019-12-07 22:41:39 +0800

    format code

--- src/routes/alert.ts
+++ src/routes/alert.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import bot from '../telegrambot/bot';
-import * as path from "path";
+import * as path from 'path';
 require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const router = express.Router();
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
   //req.body.text
+  // eslint-disable-next-line
   console.log(req.body.text);
   bot.sendMessage(`${process.env.TVINDICATORALERTBOT}`, `${req.body.text}`);
   bot.sendMessage(`${process.env.BTCGROUP}`, `${req.body.text}`);
