09374867d30f33fcb0c3d1338d1c93ebceaf3e19
Weiyi Ma <ma823285807@gmail.com>
2019-12-15 13:14:34 +0800

    update travis ci

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm .env
-touch .env
-echo HTTPS_PORT=${HTTPS_PORT} >.env
-echo HTTP_PORT=${HTTP_PORT} >>.env
-echo TOKEN=${TOKEN} >>.env
-echo TVINDICATORALERTBOT=${TVINDICATORALERTBOT} >>.env
-echo BTCGROUP=${BTCGROUP} >>.env
